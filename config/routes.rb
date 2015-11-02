5ac2b07f2a3e2d1a0138f5b78bfe49ccef89bcda
Matthew Hadley <matthew.g.hadley@gmail.com>
2015-11-02 04:57:32 +0000

    fixing route

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   root 'application#hello'
-  get pages/home
+  get 'pages/home'
   
 
   # Example of regular route:
